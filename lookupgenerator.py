e2709d94e34b44029a78409d8d64c24c73a567f4
Tony Du <tonydu121@hotmail.com>
2022-04-24 12:33:20 -0700

    remove extra files

--- lookupgenerator.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-def main():
-    from lookup import lookup
-    import json
-    with open("lookup.json", "w") as file:
-        json.dump(lookup, file, indent=2)
-
-if __name__ == '__main__':
-    main()
